595b9291f2ab64f0b306eef1cc7a38022b4cbb87
hipe <135000484+huyhieple@users.noreply.github.com>
2025-03-10 09:14:56 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -12,7 +12,7 @@ const mysql = require('mysql2/promise');
 const pool = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "Lhh37440123.",
+    password: "xxxxxxxxxxx",
     database: "readfun",
     waitForConnections: true,
     connectionLimit: 10,
